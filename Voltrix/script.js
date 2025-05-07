9001710c278862301dca58808d95617de4f35baa
Nathaniel Abed Kianto <bboynathanil@gmail.com>
2025-05-07 10:28:09 +0700

    Add files via upload

---
+++ Voltrix/script.js
@@ -0,0 +1,7 @@
+document.getElementById('payBtn').addEventListener('click', function() {
+    alert('Pay button clicked!');
+});
+
+document.getElementById('topUpBtn').addEventListener('click', function() {
+    alert('Top Up button clicked!');
+});
