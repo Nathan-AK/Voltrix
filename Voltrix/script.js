93410c116bb7382dfe9afefe93ee6580f52029bb
Nathaniel Abed Kianto <bboynathanil@gmail.com>
2025-05-30 14:21:28 +0700

    Delete Voltrix directory

--- Voltrix/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('payBtn').addEventListener('click', function() {
-    alert('Pay button clicked!');
-});
-
-document.getElementById('topUpBtn').addEventListener('click', function() {
-    alert('Top Up button clicked!');
-});
